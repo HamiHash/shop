739a80189f11166edaf0228dae2253f22ab2e0a1
HamiHash <hami.hash@yahoo.com>
2023-04-22 01:06:02 +0330

    axios setup

---
+++ app/helpers/callApi.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const callApi = () => {
+  const axiosIstance = axios.create({
+    baseURL: "http://localhost:5000/api",
+  });
+
+  axiosIstance.interceptors.request.use(
+    (config) => {
+      return config;
+    },
+    (err) => Promise.reject(err)
+  );
+
+  axiosIstance.interceptors.response.use(
+    (res) => {
+      // manage validation
+      return res;
+    },
+    (err) => Promise.reject(err)
+  );
+
+  return axiosIstance;
+};
+
+export default callApi;
